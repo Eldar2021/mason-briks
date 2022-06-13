ef1e3b00ff700c6bb2514ca288446d08d1598fd2
eldiiar Almazbek <eldiiaralmazbekov@gmail.com>
2022-06-13 12:08:44 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/mason/briks/org/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mason.briks.org.mason_briks
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:mason_briks/app/app.dart';
+import 'package:mason_briks/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
